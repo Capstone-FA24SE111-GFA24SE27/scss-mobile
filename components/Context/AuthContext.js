b3f09bb09be826b7125e0059b0985bda3d69147e
khanhnguyen27 <nak272625@gmail.com>
2024-09-06 21:43:39 +0700

    Basic UI for Login/SignUp/Home/Schedule

---
+++ components/Context/AuthContext.js
@@ -0,0 +1,77 @@
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { createContext, useEffect, useState } from "react";
+
+// Tạo context
+export const AuthContext = createContext();
+
+// Tạo provider
+export const AuthProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(false);
+  //   const [userData, setUserData] = useState(null);
+  //   const [session, setSession] = useState(null);
+
+  //   useEffect(() => {
+  //     const checkUserSession = async () => {
+  //       if (isLogin === false) {
+  //         setIsLogin(true);
+  //       }
+  //   const userDataAsync = await AsyncStorage.getItem('userData');
+  //   const sessionAsync = await AsyncStorage.getItem('session');
+  //   if (userDataAsync !== null && sessionAsync !== null) {
+  //     const sessionAfterParse = JSON.parse(sessionAsync);
+  //     const userDataAfterParse = JSON.parse(userDataAsync);
+  //     const currentTime = new Date().getTime();
+  //     console.log(sessionAfterParse.expires_at > currentTime)
+  //     if (sessionAfterParse.expires_at > currentTime) {
+  //       setUserData(userDataAfterParse);
+  //       setSession(sessionAfterParse);
+  //       setIsLogin(true);
+  //     }
+  //   }
+  //   console.log("Async")
+  //   console.log(JSON.stringify(JSON.parse(userDataAsync), undefined, 4))
+  //   console.log(JSON.stringify(JSON.parse(sessionAsync), undefined, 4))
+  //     };
+
+  //     checkUserSession();
+  //   }, []);
+
+  //   const login = async (userInfo, sessionInfo) => {
+  //     setUserData(userInfo);
+  //     setSession(sessionInfo)
+  //     console.log(JSON.stringify(userInfo, undefined, 4))
+  //     console.log(JSON.stringify(sessionInfo, undefined, 4))
+  //     await AsyncStorage.setItem('userData', JSON.stringify(userInfo));
+  //     await AsyncStorage.setItem('session', JSON.stringify(sessionInfo));
+  //     const userDataAsync = await AsyncStorage.getItem('userData');
+  //     const sessionAsync = await AsyncStorage.getItem('session');
+  //     console.log(JSON.stringify(JSON.parse(userDataAsync), undefined, 4))
+  //     console.log(JSON.stringify(JSON.parse(sessionAsync), undefined, 4))
+  //     setIsLogin(true);
+  //   };
+
+  const login = () => {
+    setIsLogin(true);
+  };
+
+  const logout = () => {
+    setIsLogin(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLogin,
+        login,
+        logout,
+        setIsLogin,
+        // userData,
+        // setUserData,
+        // setSession,
+        // session,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,201 @@
+import React, { useContext, useEffect, useRef } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  Animated,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { AuthContext } from "../Context/AuthContext";
+import Login from "../Screen/Login";
+import SignUp from "../Screen/SignUp";
+import Home from "../Screen/Home";
+import GettingStart from "../Screen/GettingStart";
+import Schedule from "../Screen/Schedule";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  const { isLogin } = useContext(AuthContext);
+
+  const screenOptions = () => ({
+    headerShown: false,
+    tabBarStyle: {
+      height: 60,
+      // position: "absolute",
+      // margin: 16,
+      // borderRadius: 16,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+
+  const TabArray = [
+    {
+      route: "Home",
+      label: "Home",
+      active: "home",
+      inActive: "home-outline",
+      component: Home,
+    },
+    {
+      route: "Schedule",
+      label: "Schedule",
+      active: "calendar",
+      inActive: "calendar-outline",
+      component: Schedule,
+    },
+  ];
+
+  const TabButton = (props) => {
+    const { item, onPress, accessibilityState } = props;
+    const focused = accessibilityState.selected;
+    
+    const viewScaleAnim = useRef(new Animated.Value(focused ? 1 : 0)).current;
+    const textScaleAnim = useRef(new Animated.Value(focused ? 1 : 0)).current;
+
+    useEffect(() => {
+      if (focused) {
+        Animated.parallel([
+          Animated.timing(viewScaleAnim, {
+            toValue: 1,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(textScaleAnim, {
+            toValue: 1,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+        ]).start();
+      } else {
+        Animated.parallel([
+          Animated.timing(viewScaleAnim, {
+            toValue: 0,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+          Animated.timing(textScaleAnim, {
+            toValue: 0,
+            duration: 400,
+            useNativeDriver: true,
+          }),
+        ]).start();
+      }
+    }, [focused]);
+
+    return (
+      <TouchableOpacity
+        onPress={onPress}
+        activeOpacity={1}
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          height: 60,
+        }}
+      >
+        <View>
+          <Animated.View
+            style={[ StyleSheet.absoluteFillObject, {
+              transform: [{ scale: viewScaleAnim }],
+              backgroundColor: focused ? "#ff469e" : "white",
+              borderRadius: 16,
+            }]}
+          />
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              padding: 8,
+              borderRadius: 16,
+            }}
+          >
+            <Ionicons
+              name={item.active}
+              size={24}
+              color={focused ? "white" : "black"}
+            />
+            <Animated.View style={{ transform: [{ scale: textScaleAnim }] }}>
+              {focused && (
+                <Text
+                  style={{
+                    color: "white",
+                    paddingHorizontal: 10,
+                    fontSize: 16,
+                    fontWeight: 600,
+                  }}
+                >
+                  {item.label}
+                </Text>
+              )}
+            </Animated.View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const tabOptions = {
+    tabBarButton: () => null,
+    tabBarStyle: { display: "none" },
+  };
+
+  return (
+    <>
+      {isLogin === false ? (
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="GettingStart"
+        >
+          <Stack.Screen name="GettingStart" component={GettingStart} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </Stack.Navigator>
+      ) : (
+        <Tab.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+          {TabArray.map((item, index) => {
+            return (
+              <Tab.Screen
+                key={index}
+                name={item.route}
+                component={item.component}
+                options={{
+                  tabBarButton: (props) => <TabButton {...props} item={item} />,
+                }}
+              />
+            );
+          })}
+          {/* <Tab.Screen name="Notification" component={Notification} options={tabOptions} />
+          <Tab.Screen
+            name="Personal"
+            component={Personal}
+            options={tabOptions}
+          />
+          <Tab.Screen name="Profile" component={Profile} options={tabOptions} />
+          <Tab.Screen
+            name="EditProfile"
+            component={EditProfile}
+            options={tabOptions}
+          />
+          <Tab.Screen
+            name="ChangePassword"
+            component={ChangePassword}
+            options={tabOptions}
+          />
+          <Tab.Screen
+            name="DeleteAccount"
+            component={DeleteAccount}
+            options={tabOptions}
+          />
+          <Tab.Screen name="PT" component={PT} options={tabOptions} /> */}
+        </Tab.Navigator>
+      )}
+    </>
+  );
+}
